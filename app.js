b362f036c9df39656f637a770e8812ee3b034541
massytak <massi.messalti@gmail.com>
2021-07-16 18:45:47 +0200

    maj app.js

--- app.js
+++ app.js
@@ -8,19 +8,24 @@ const hbs          = require('hbs');
 const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
+const cors = require('cors');
 
-
+/// mongoose
 mongoose
-  .connect('mongodb://localhost/serveurnode', {useNewUrlParser: true})
-  .then(x => {
-    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+  .connect(process.env.MONGODB_URL,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
   })
-  .catch(err => {
-    console.error('Error connecting to mongo', err)
+  .then((x) => {
+    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`);
+  })
+  .catch((err) => {
+    console.error("Error connecting to mongo", err);
   });
 
-const app_name = require('./package.json').name;
-const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
+const app_name = require("./package.json").name;
+const debug = require("debug")(`${app_name}:${path.basename(__filename).split(".")[0]}`);
 
 const app = express();
 
@@ -49,7 +54,7 @@ app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 // default value for title local
 app.locals.title = 'Express - Generated with IronGenerator';
 
-
+//routes
 
 const index = require('./routes/index');
 app.use('/', index);

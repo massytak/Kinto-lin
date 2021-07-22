5d3511bb992b335a460ace2fde98a2c1f2f1f34f
massytak <massi.messalti@gmail.com>
2021-07-22 23:48:54 +0200

    ajout de referencing non termine

--- routes/reviews.routes.js
+++ routes/reviews.routes.js
@@ -9,50 +9,58 @@ const Reviews = require("../models/Reviews.model");
 
 ////////////////////////////Create/////////////////////////////
 router.post("/create", (req, res, next) => {
-  console.log( req.session.currentUser._id);
-  const user = req.session.currentUser._id;
+  
+
+  
   const message = req.body.message;
   const gameIdFromMongo = req.body.gameIdFromMongo;
-  const review = new Reviews({
-    user,
-    message,
-    gameIdFromMongo,
-  });
-  review
-    .save()
-    .then((review) => {
-      console.log('Review created', review);
-      res.status(200).json(review);
-    })
-    .catch((err) => {
-      console.log("Error to create review");
-      res.status(403).json({ message: 'Review not created' });
+
+  if (!req.session.currentUser) {
+    res.status(400).json({ message: "you need to login" });
+    return;
+  }
+  if (req.session.currentUser){
+    const user = req.session.currentUser._id;
+    const review = new Reviews({
+      user,
+      message,
+      gameIdFromMongo,
     });
+    review
+      .save()
+      .then((review) => {
+        console.log("Review created", review);
+        res.status(200).json(review);
+      })
+      .catch((err) => {
+        console.log("Error to create review");
+        res.status(403).json({ message: "Review not created" });
+      });
+  }
 });
 
 ////////////////////////////Read/////////////////////////////
 router.get("/read/:id", (req, res, next) => {
   console.log(req.params.id);
   Reviews.findById(req.params.id)
-    .then((review) =>  res.status(200).json(review))
+    .then((review) => res.status(200).json(review))
     .catch((err) => next(err));
 });
 
 ////////////////////////////Update////////////////////////////
-router.put('/update/:id',(req,res,next)=>{
-  Reviews.findByIdAndUpdate({_id:req.params.id},req.body)
-  .then((review)=>{
-      Reviews.findOne({_id:req.params.id}).then((review)=>{
-        res.send({review}) 
-      })
-  })
-})
+router.put("/update/:id", (req, res, next) => {
+  Reviews.findByIdAndUpdate({ _id: req.params.id }, req.body).then((review) => {
+    Reviews.findOne({ _id: req.params.id }).then((review) => {
+      res.send({ review });
+    });
+  });
+});
 
 ////////////////////////////Delete/////////////////////////////
 router.delete("/delete/:id", (req, res, next) => {
   console.log(req.params.id);
   Reviews.findByIdAndRemove(req.params.id)
-    .then((review) =>  res.status(200).json(review))
+    .then((review) => res.status(200).json(review))
     .catch((err) => next(err));
 });
 

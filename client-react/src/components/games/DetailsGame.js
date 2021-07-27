e482f1a2c8904698010e909cc26577123c04462d
massytak <massi.messalti@gmail.com>
2021-07-27 20:50:05 +0200

    player video loop

--- client-react/src/components/games/DetailsGame.js
+++ client-react/src/components/games/DetailsGame.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import Comment from "./reviews/Comment";
 import { detailofGame } from "./game-service";
-
+import ReactPlayer from 'react-player'
 class DetailsGame extends Component {
   state = {
     game: false,
@@ -21,10 +21,11 @@ class DetailsGame extends Component {
     return (
       (!this.state.game && <h1>Loading...</h1>) || (
         <div className="single-game">
-          <video autoPlay loop>
+          {/* <video autoPlay loop>
             <source src={this.state.game.trailer} />
             Your browser does not support the video tag.
-          </video>
+          </video> */}
+          <ReactPlayer url={this.state.game.trailer} loop playing />
 
           <img src={this.state.game.thumbnail} alt="game" />
           <h3>{this.state.game.title}</h3>

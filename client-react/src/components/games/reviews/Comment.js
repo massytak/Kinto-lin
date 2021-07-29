b6a8fda01d66c4f1de31c2bb2ad833d987ec5722
tcw-cyborg <tcwong007@gmail.com>
2021-07-29 21:24:54 +0200

    reviews in progress

--- client-react/src/components/games/reviews/Comment.js
+++ client-react/src/components/games/reviews/Comment.js
@@ -1,14 +1,43 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import RatingStars from "./RatingStars";
 
 class Comment extends Component {
   render() {
     return (
-      <div>
+      <div style={styles.container}>
+        <h2>Commentaires :</h2>
         <RatingStars />
+        <textarea placeholder="Vos commentaires" style={styles.textarea} />
+        <button style={styles.button}>Soumettre</button>
       </div>
     );
   }
 }
+
+const styles = {
+  container: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  textarea: {
+    border: "1px solid #2C3E50",
+    borderRadius: 5,
+    width: 300,
+    margin: "20px 0",
+    minHeight: 100,
+    padding: 10,
+    backgroundColor: "#2C3E50",
+    textColor: "#00FF00",
+  },
+  button: {
+    border: "1px solid #2C3E50",
+    borderRadius: 5,
+    width: 300,
+    padding: 10,
+    backgroundColor: "#2C3E50",
+    textColor: "#00FF00",
+  },
+};
 
 export default Comment;
--- client-react/src/components/games/reviews/RatingStars.js
+++ client-react/src/components/games/reviews/RatingStars.js
@@ -1,25 +1,26 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import ReactStars from "react-rating-stars-component";
 
 class RatingStars extends Component {
-  state={rate:0}
- ratingChanged = (rating) => {
+  state = { rate: 0 };
+  ratingChanged = (rating) => {
     console.log(`You have given ${rating} star rating for us.`);
-    this.setState({rate:rating})
+    this.setState({ rate: rating });
   };
 
   render() {
     return (
       <div className="App">
-      <ReactStars
-        activeColor="#00FF00"
-        size={100}
-        count={5}
-        onChange={this.ratingChanged}
-      />
-    </div>
-    )
+        <ReactStars
+          color="#2C3E50"
+          activeColor="#00FF00"
+          size={50}
+          count={5}
+          onChange={this.ratingChanged}
+        />
+      </div>
+    );
   }
 }
 
-export default RatingStars
+export default RatingStars;

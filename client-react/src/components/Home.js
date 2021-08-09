3bff18932bb8611946d2ae6e0136bf5ad0759956
sofiaould <sofiaouldabderrahmane@yahoo.fr>
2021-08-09 17:30:24 +0200

    edit game done

--- client-react/src/components/Home.js
+++ client-react/src/components/Home.js
@@ -1,15 +1,20 @@
 /** @format */
 
 import React, { Component } from "react";
+
 class Home extends Component {
   render() {
     return (
       <>
         {this.props.userInSession ? (
-          <p>Bonjour {this.props.userInSession.username} </p>
+          <p className="bienvenue">Bonjour {this.props.userInSession.username} </p>
         ) : (
-          <div>Bienvenue sur la home page</div>
+          <div>
+            <p>Bienvenue sur la home page</p>
+            <img src="https://img.favpng.com/18/9/21/sun-wukong-art-dota-2-drawing-t-shirt-png-favpng-qZyjtcL2CLtNKUcfRT5nspLeu.jpg" alt="kinto-un logo"></img>
+          </div>
         )}
+   
       </>
     );
   }

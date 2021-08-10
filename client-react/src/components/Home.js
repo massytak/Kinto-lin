9abcb42fe6e1e7284effaa2a285b5eaba0540450
sofiaould <sofiaouldabderrahmane@yahoo.fr>
2021-08-10 19:09:02 +0200

    ok

--- client-react/src/components/Home.js
+++ client-react/src/components/Home.js
@@ -5,6 +5,7 @@ import React, { Component } from "react";
 import singeKinto from "../Styling/singe-kinto-un.png";
 import Login from "./auth/Login";
 import Signup from "./auth/Signup";
+import "../Styling/Home.css";
 class Home extends Component {
   state = {
     connectIn: this.props.userInSession,
@@ -34,15 +35,16 @@ class Home extends Component {
   render(props) {
     const divStyle = {
       paddingTop: "5em",
+
     };
     return (
       <div style={divStyle}>
-        <img src={singeKinto} alt="sigep" />
+        <img classeName="singe" src={singeKinto} alt="sigep" />
         {this.props.userInSession ? (
           <p className="bienvenue">Bonjour {this.props.userInSession.username} </p>
         ) : (
           <div>
-            <div>Bienvenue sur la home page</div>
+            <div classeName="bienvenue">Bienvenue sur la home page</div>
             <button onClick={this.activeLogin}>login</button>
             <button onClick={this.activeSginup}>Signup</button>
             {this.state.login && (
